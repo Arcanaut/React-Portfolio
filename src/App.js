6967260e9ef643d66b4d952cdb96b4c9477851ec
Brendan Ahearn <91020994+Arcanaut@users.noreply.github.com>
2022-09-03 18:05:29 -0500

    Created Hero Header
    May move contact links to separate page with later 'contact me' form.

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@
 import { IconButton } from "@chakra-ui/button";
 import { useColorMode } from "@chakra-ui/color-mode";
 import { Flex, VStack, Heading, Spacer } from "@chakra-ui/layout";
-import { FaSun, FaMoon } from "react-icons/fa";
-
+import { FaSun, FaMoon, FaGithub, FaLinkedin } from "react-icons/fa";
+import Header from "./components/Header/header";
+// import Portfolio from "./components/Portfolio";
+// import Contact from "./components/Contact";
 function App() {
    
   const { colorMode, toggleColorMode } = useColorMode(); 
@@ -12,14 +14,22 @@ function App() {
     <VStack>
       <Flex w="100%">
         <Heading ml="2" size="md" fontWeight='extrabold' 
-        color='blue.500'>Home</Heading>
-        
+        color='cyan.500'>Home</Heading>
+
         <Spacer></Spacer>
        <IconButton ml={9} icon={isDark ? <FaSun /> : <FaMoon />} 
        isRound="true" onClick={toggleColorMode}></IconButton>
-      </Flex>
+     
+      <IconButton ml={4} icon={<FaGithub/>} 
+       isRound="true" onClick={() => window.open("https://github.com/Arcanaut/React-Portfolio")}></IconButton>
+       <IconButton ml={4} icon={<FaLinkedin/>} 
+       isRound="true" onClick={() => window.open("https://www.linkedin.com/in/brendan-ahearn-476688a5/  ")}></IconButton> 
+       </Flex>
+       <Header></Header>
+       {/* <Portfolio></Portfolio>
+       <Contact></Contact> */}
     </VStack>
   );
 }
-   
+
 export default App;
--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,10 +1,35 @@
 import React from 'react';
+import { Stack, Circle, Flex, Box, Text } from '@chakra-ui/layout';
+import { Button } from '@chakra-ui/button';
+import { Image } from '@chakra-ui/image';
+import {useColorMode} from '@chakra-ui/color-mode'
+import { useMediaQuery } from '@chakra-ui/react';
+
+
 
 function Header() {
+    const { colorMode } = useColorMode(); 
+    const isDark = colorMode === "dark";
+    const [isNotSmallerScreen] = useMediaQuery("(min-width-600px)");
     return(
-        <div>
-
-        </div>
+        <Stack>
+ <Circle position="absolute" bg="blue.100" opacity="0.1" w="300px" h="300px" alignSelf="flex-end"></Circle>
+ <Flex direction={isNotSmallerScreen ? "row" : "column"}
+                spacing="200px" p={isNotSmallerScreen ? "32" : "0"}
+                alignSelf="flex-start">
+                    <Box mt={isNotSmallerScreen? "0" : 16} align="flex-start">
+                        <Text fontSize="5xl" fontWeight="semibold">Hi, I'm</Text>
+                        <Text fontSize="7xl" fontWeight="bold" bgGradient="linear(to-r, cyan.400, blue.500, purple.600)" bgClip='text' >Brendan Ahearn</Text>
+                            <Button mt={8} colorScheme="blue" onClick={() => window.open("https://github.com/Arcanaut")}>Github
+                            </Button>
+                    </Box>
+                    <Image alignSelf="center" mt={isNotSmallerScreen ? "0" : "12"}
+                    mb={isNotSmallerScreen ? "0" : "12"} borderRadius='full'
+                    backgroundColor="transparent" boxShadow="lg"
+                    boxSize="300px" src='https://cdn.discordapp.com/attachments/968688052820140104/1015309067645898842/unknown.png'
+                    />
+                    </Flex>
+        </Stack>
     )
 }
 
