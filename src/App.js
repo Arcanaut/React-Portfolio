b18feb499129d6abb2b8171d8d9320631b2c4cf7
Brendan Ahearn <91020994+Arcanaut@users.noreply.github.com>
2022-09-20 19:10:13 -0500

    Fixed Home Route
    converted 'home' to landing page route while keeping the styling.
    
    Also moved icon-buttons to "Header" component (possibly temporarily) so it renders below the scroll on landing page.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { IconButton } from "@chakra-ui/button";
 import { useColorMode } from "@chakra-ui/color-mode";
 import { Flex, VStack, Heading } from "@chakra-ui/layout";
-import { FaSun, FaMoon, FaGithub, FaLinkedin } from "react-icons/fa";
+import { FaSun, FaMoon, } from "react-icons/fa";
 import Header from "./components/Header/header";
 import { NavLink, Link } from "react-router-dom";
 // import About from "./About";
@@ -11,7 +11,7 @@ import { NavLink, Link } from "react-router-dom";
 // import Contact from "./components/Contact";
 
 
-// TODO: potentially move Nav for PWA in to App.js instead of own 
+// TODO: potentially move Nav for PWA in to App.js instead of own component
 //TODO: Get NPM packages from fontawesome for language icons
 function App() {
    
@@ -22,7 +22,11 @@ function App() {
       <VStack p={5}>
         <Flex w="100%" display="flex" justifyContent="space-around" alignItems="center">
           <Heading ml="8" size="md" fontWeight='extrabold' 
-          color='cyan.500'>Home</Heading>
+          color='cyan.500'>         
+              <NavLink exact activeClassName="active" to="/">
+                  <Link to="/">Home</Link>
+              </NavLink>
+          </Heading>
 
            <NavLink exact activeClassName="active" to="/about">
               <Link to="/about">about</Link>
@@ -41,10 +45,7 @@ function App() {
         <IconButton ml={9} icon={isDark ? <FaSun /> : <FaMoon />} 
         isRound="true" onClick={toggleColorMode}></IconButton>
       
-        <IconButton ml={4} icon={<FaGithub/>} 
-        isRound="true" onClick={() => window.open("https://github.com/Arcanaut")}></IconButton>
-        <IconButton ml={4} icon={<FaLinkedin/>} 
-        isRound="true" onClick={() => window.open("https://www.linkedin.com/in/brendan-ahearn-476688a5/")}></IconButton> 
+         
         </Flex>
         <Header></Header>
         {/* <Portfolio></Portfolio>
--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { IconButton } from "@chakra-ui/button";
+import {FaGithub, FaLinkedin, } from "react-icons/fa";
 import { Stack, Circle, Flex, Box, Text } from '@chakra-ui/layout';
 import { Button } from '@chakra-ui/button';
 import { Image } from '@chakra-ui/image';
@@ -48,6 +50,11 @@ function Header() {
                             </Box>
                             <Button mt={8} colorScheme="blue" onClick={() => window.open("https://docs.google.com/document/d/11pqyqCb4AtIcOBAPoHOvd2RLn8vaI2Aec0z2zG7VciI/edit?usp=sharing")}>Resume
                             </Button>
+
+                            <IconButton ml={4} icon={<FaGithub/>} 
+        isRound="true" onClick={() => window.open("https://github.com/Arcanaut")}></IconButton>
+        <IconButton ml={4} icon={<FaLinkedin/>} 
+        isRound="true" onClick={() => window.open("https://www.linkedin.com/in/brendan-ahearn-476688a5/")}></IconButton>
                     </Box>
                     
                     </Flex>
