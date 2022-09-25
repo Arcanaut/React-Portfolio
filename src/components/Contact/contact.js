3da4d58982f9b2a7dc43c393fa852fb97cd47b24
Brendan Ahearn <91020994+Arcanaut@users.noreply.github.com>
2022-09-25 16:25:09 -0500

    contact mvp functionality working
    still requires styling, as well as possible QoA implementations e.g. clearing of fields, and notification that email has been sent.

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -2,46 +2,43 @@ import { React, useRef } from 'react';
 import emailjs from "@emailjs/browser";
 
  //chakra UI styling
-// import {
-//   Box,
-//   Button,
-//   Flex,
-//   FormControl,
-//   FormLabel,
-//   Heading,
-//   IconButton,
-//   Input,
-//   InputGroup,
-//   InputLeftElement,
-//   Link,
-//   Stack,
-//   Textarea,
-//   Tooltip,
-//   useClipboard,
-//   useColorModeValue,
-//   VStack
-// } from "@chakra-ui/react";
+import {
+  Box
+} from "@chakra-ui/react";
+
 // import { BsGithub, BsLinkedin, BsPerson } from "react-icons/bs";
 // import { MdEmail, MdOutlineEmail } from "react-icons/md";
 
 
 function Contact(){
-
+    const form =useRef();
     const sendEmail = (e) => {
         e.preventDefault();
     
-        emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_PUBLIC_KEY')
-          .then((result) => {
-              console.log(result.text);
-          }, (error) => {
-              console.log(error.text);
-          });
-      };
+        emailjs.sendForm('service_o9zp49j', 
+            'contact_form', 
+            form.current, 
+            '1spzpYZW8x2zRzRm5')
+                .then((result) => {    
+                    console.log(result.text);
+                    }, 
+                        (error) => {
+                        console.log(error.text);
+                    });
+                };
 
     return (
-        <div>
-            Contact Pages
-        </div>
+        <Box>
+      <form ref={form} onSubmit={sendEmail}>
+        <label>Name</label>
+        <input type="text" name="user_name" />
+        <label>Email</label>
+        <input type="email" name="user_email" />
+        <label>Message</label>
+        <textarea name="message" />
+        <input type="submit" value="Send" />
+      </form>
+    </Box>
     );
 };
 
