ac82064fc071fd1e875b2b166519dd7265eb4427
Brendan Ahearn <91020994+Arcanaut@users.noreply.github.com>
2022-10-27 16:24:48 -0500

    changed browserRouter to hashRouter for ghpages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import { BrowserRouter as Router, Routes, Route, } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, } from "react-router-dom";
 
 import { Stack } from "@chakra-ui/layout";
 
